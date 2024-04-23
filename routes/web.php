9d1fcdfb7e7ad90f9d76eaa1128b3ef7245ac963
momchiloiliev <momchilo.iliev@students.finki.ukim.mk>
2024-04-23 23:43:33 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -76,5 +76,6 @@ Route::get('categories/{category:slug}', function(Category $category) {
 Route::get('authors/{author:username}', function(User $author) {
     return view('posts', [
         'posts' => $author->posts
+
     ]);
 });
