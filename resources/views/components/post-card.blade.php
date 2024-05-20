30cbb0fdf068048f0fd2e7981fe703c7aab622d1
momchiloiliev <momchilo.iliev@students.finki.ukim.mk>
2024-05-20 23:15:27 +0200

    test

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -46,6 +46,8 @@
                     </div>
                 </div>
 
+
+
                 <div class="hidden lg:block">
                     <a href="/posts/{{ $post->slug }}"
                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
